40f55d01d67f2b267b12df3f0e4dc5fdf2fd6a4e
natashalynne <hakun.natasha@gmail.com>
2015-10-16 13:02:23 -0400

    Iterate through search array.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 $(function(){
-    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    console.log(data);
-  });
+  //   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+  //   console.log(data);
+  // });
 
-    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    myData = data.Search[0].Title;
-    console.log(myData);
+  //   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+  //   myData = data.Search[0].Title;
+  //   console.log(myData);
+  // });
+
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    myData = data.Search;
+    $.each(myData, function(index,value){
+      console.log(value.Title);
+    });
   });
 });
