5d7ee1f536b5090979e87bc870b7e30ca29267a5
natashalynne <hakun.natasha@gmail.com>
2015-10-16 13:00:20 -0400

    Add myData.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ $(function(){
     $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
     console.log(data);
   });
+
+    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    myData = data.Search[0].Title;
+    console.log(myData);
+  });
 });
