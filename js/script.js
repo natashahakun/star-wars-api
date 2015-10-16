2b5514fbb25055d153bfed70ebcf6cbd73fd1805
natashalynne <hakun.natasha@gmail.com>
2015-10-16 13:14:49 -0400

    Add getRequest and showResults functions.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+// $(function(){
   //   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
   //   console.log(data);
   // });
@@ -8,10 +8,40 @@ $(function(){
   //   console.log(myData);
   // });
 
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    myData = data.Search;
-    $.each(myData, function(index,value){
-      console.log(value.Title);
-    });
+  // $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+  //   myData = data.Search;
+  //   $.each(myData, function(index,value){
+  //     console.log(value.Title);
+  //   });
+  // });
+// });
+
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
   });
 });
+
+function getRequest(searchTerm){
+  var params = {
+    s: searchTerm,
+    r: 'json'
+  };
+  url = 'http://www.omdbapi.com';
+
+  $.getJSON(url, params, function(data){
+    showResults(data.Search);
+  });
+}
+
+function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
+
